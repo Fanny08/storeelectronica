d046ca28a79888812db5129c2636739846fdc97b
Luis Acxis <luisacxis@gmail.com>
2019-11-25 06:54:02 -0600

    add funciones con productos

---
+++ php/productos/getInfo.php
@@ -0,0 +1,34 @@
+<?php
+include('../Conexion.php');
+include('../Funciones.php');
+
+$id = filter_var($_POST['id'],FILTER_SANITIZE_NUMBER_INT);
+
+if(vacio($id))
+{
+	$obj = new stdClass();
+	$consulta = $conexion -> query("SELECT Id, Nombre, Estrellas, Precio, Stock, Descripcion FROM Productos WHERE Id = {$id}");
+	while($fila = $consulta -> fetch_assoc())
+	{
+		$imagenes = [];
+		
+		$imagenesSQL = $conexion -> query("SELECT Nombre FROM Imagenes WHERE Productos_id = {$fila['Id']}");
+		while($filaImagenes = $imagenesSQL -> fetch_assoc()) {
+			array_push($imagenes, $filaImagenes['Nombre']);
+		}
+		
+		$obj -> productos[] =
+		[
+			"id" => $fila['Id'],
+			"nombre" => $fila['Nombre'],
+			"estrellas" => $fila['Estrellas'],
+			"precio" => $fila['Precio'],
+			"stock" => $fila['Stock'],
+			"descripcion" => $fila['Descripcion'],
+			"imagenes" => $imagenes
+		];
+	}
+}
+
+echo json_encode($obj);
+?>

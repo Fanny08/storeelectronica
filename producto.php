fadd4aacf411a208224cf995e97d56d4e9eea6df
Luis Acxis <luisacxis@gmail.com>
2019-11-25 06:55:20 -0600

    add comentarios de facebook

--- producto.php
+++ producto.php
@@ -7,6 +7,9 @@
     </head>
 	<body>
 	
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v5.0&appId=202902796950543"></script>
+	
 		<?php
 		include('html-source/header.html');
 		?>
@@ -18,71 +21,29 @@
 				<!-- row -->
 				<div class="row">
 					<!-- Product main img -->
-					<div class="col-md-5 col-md-push-2">
+					<div class="col-md-4">
 						<div id="product-main-img">
 							<div class="product-preview">
-								<img src="./img/product01.png" alt="">
-							</div>
-
-							<div class="product-preview">
-								<img src="./img/product03.png" alt="">
-							</div>
-
-							<div class="product-preview">
-								<img src="./img/product06.png" alt="">
-							</div>
-
-							<div class="product-preview">
-								<img src="./img/product08.png" alt="">
+								<img id="img-main" alt="">
 							</div>
 						</div>
 					</div>
 					<!-- /Product main img -->
 
-					<!-- Product thumb imgs -->
-					<div class="col-md-2  col-md-pull-5">
-						<div id="product-imgs">
-							<div class="product-preview">
-								<img src="./img/product01.png" alt="">
-							</div>
-
-							<div class="product-preview">
-								<img src="./img/product03.png" alt="">
-							</div>
-
-							<div class="product-preview">
-								<img src="./img/product06.png" alt="">
-							</div>
-
-							<div class="product-preview">
-								<img src="./img/product08.png" alt="">
-							</div>
-						</div>
-					</div>
-					<!-- /Product thumb imgs -->
-
 					<!-- Product details -->
-					<div class="col-md-5">
+					<div class="col-md-4">
 						<div class="product-details">
-							<h2 class="product-name">product name goes here</h2>
+							<h2 id="product-name" class="product-name"></h2>
 							<div>
-								<div class="product-rating">
-									<i class="fa fa-star"></i>
-									<i class="fa fa-star"></i>
-									<i class="fa fa-star"></i>
-									<i class="fa fa-star"></i>
-									<i class="fa fa-star-o"></i>
-								</div>
-								<a class="review-link" href="#">10 Comentario(s)</a>
+								<div id="product-rating" class="product-rating"></div>
 							</div>
 							<div>
-								<h3 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h3>
-								<span class="product-available">In Stock</span>
+								<h3 class="product-price"><span id="product-price"></span> <del id="product-old-price" class="product-old-price"></del></h3>
 							</div>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+							<p id="product-descripcion"></p>
 
-							<div class="add-to-cart">
-								<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+							<div id="add-to-cart-btn" class="add-to-cart">
+								<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> <span id="text-btn-loading">Cargando Producto...</span></button>
 							</div>
 
 						</div>
@@ -90,184 +51,8 @@
 					<!-- /Product details -->
 
 					<!-- Product tab -->
-					<div class="col-md-12">
-						<div id="product-tab">
-
-							<!-- product tab content -->
-							<div class="tab-content">
-
-								<!-- tab3  -->
-								<div id="tab3" class="tab-pane fade in active">
-									<div class="row">
-										<!-- Rating -->
-										<div class="col-md-3">
-											<div id="rating">
-												<div class="rating-avg">
-													<span>4.5</span>
-													<div class="rating-stars">
-														<i class="fa fa-star"></i>
-														<i class="fa fa-star"></i>
-														<i class="fa fa-star"></i>
-														<i class="fa fa-star"></i>
-														<i class="fa fa-star-o"></i>
-													</div>
-												</div>
-												<ul class="rating">
-													<li>
-														<div class="rating-stars">
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-														</div>
-														<div class="rating-progress">
-															<div style="width: 80%;"></div>
-														</div>
-														<span class="sum">3</span>
-													</li>
-													<li>
-														<div class="rating-stars">
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star-o"></i>
-														</div>
-														<div class="rating-progress">
-															<div style="width: 60%;"></div>
-														</div>
-														<span class="sum">2</span>
-													</li>
-													<li>
-														<div class="rating-stars">
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star-o"></i>
-															<i class="fa fa-star-o"></i>
-														</div>
-														<div class="rating-progress">
-															<div></div>
-														</div>
-														<span class="sum">0</span>
-													</li>
-													<li>
-														<div class="rating-stars">
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star-o"></i>
-															<i class="fa fa-star-o"></i>
-															<i class="fa fa-star-o"></i>
-														</div>
-														<div class="rating-progress">
-															<div></div>
-														</div>
-														<span class="sum">0</span>
-													</li>
-													<li>
-														<div class="rating-stars">
-															<i class="fa fa-star"></i>
-															<i class="fa fa-star-o"></i>
-															<i class="fa fa-star-o"></i>
-															<i class="fa fa-star-o"></i>
-															<i class="fa fa-star-o"></i>
-														</div>
-														<div class="rating-progress">
-															<div></div>
-														</div>
-														<span class="sum">0</span>
-													</li>
-												</ul>
-											</div>
-										</div>
-										<!-- /Rating -->
-
-										<!-- Reviews -->
-										<div class="col-md-6">
-											<div id="reviews">
-												<ul class="reviews">
-													<li>
-														<div class="review-heading">
-															<h5 class="name">John</h5>
-															<p class="date">27 DEC 2018, 8:0 PM</p>
-															<div class="review-rating">
-																<i class="fa fa-star"></i>
-																<i class="fa fa-star"></i>
-																<i class="fa fa-star"></i>
-																<i class="fa fa-star"></i>
-																<i class="fa fa-star-o empty"></i>
-															</div>
-														</div>
-														<div class="review-body">
-															<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-														</div>
-													</li>
-													<li>
-														<div class="review-heading">
-															<h5 class="name">John</h5>
-															<p class="date">27 DEC 2018, 8:0 PM</p>
-															<div class="review-rating">
-																<i class="fa fa-star"></i>
-																<i class="fa fa-star"></i>
-																<i class="fa fa-star"></i>
-																<i class="fa fa-star"></i>
-																<i class="fa fa-star-o empty"></i>
-															</div>
-														</div>
-														<div class="review-body">
-															<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-														</div>
-													</li>
-													<li>
-														<div class="review-heading">
-															<h5 class="name">John</h5>
-															<p class="date">27 DEC 2018, 8:0 PM</p>
-															<div class="review-rating">
-																<i class="fa fa-star"></i>
-																<i class="fa fa-star"></i>
-																<i class="fa fa-star"></i>
-																<i class="fa fa-star"></i>
-																<i class="fa fa-star-o empty"></i>
-															</div>
-														</div>
-														<div class="review-body">
-															<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-														</div>
-													</li>
-												</ul>
-											</div>
-										</div>
-										<!-- /Reviews -->
-
-										<!-- Review Form -->
-										<div class="col-md-3">
-											<div id="review-form">
-												<form class="review-form">
-													<input class="input" type="text" placeholder="Your Name">
-													<input class="input" type="email" placeholder="Your Email">
-													<textarea class="input" placeholder="Your Review"></textarea>
-													<div class="input-rating">
-														<span>Your Rating: </span>
-														<div class="stars">
-															<input id="star5" name="rating" value="5" type="radio"><label for="star5"></label>
-															<input id="star4" name="rating" value="4" type="radio"><label for="star4"></label>
-															<input id="star3" name="rating" value="3" type="radio"><label for="star3"></label>
-															<input id="star2" name="rating" value="2" type="radio"><label for="star2"></label>
-															<input id="star1" name="rating" value="1" type="radio"><label for="star1"></label>
-														</div>
-													</div>
-													<button class="primary-btn">Submit</button>
-												</form>
-											</div>
-										</div>
-										<!-- /Review Form -->
-									</div>
-								</div>
-								<!-- /tab3  -->
-							</div>
-							<!-- /product tab content  -->
-						</div>
+					<div class="col-md-4">
+						<div class="fb-comments" id="facebook-comments" data-mobile="true" data-numposts="5"></div>
 					</div>
 					<!-- /product tab -->
 				</div>
@@ -277,13 +62,95 @@
 		</div>
 		<!-- /SECTION -->
 
-		<!-- jQuery Plugins -->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/slick.min.js"></script>
-		<script src="js/nouislider.min.js"></script>
-		<script src="js/jquery.zoom.min.js"></script>
-		<script src="js/main.js"></script>
+		<?php
+		include('html-source/scripts.html');
+		?>
+<script>
+jQuery(document).ready(function(){
+	
+	var loaded = false;
+	
+	var id = getVarURL('id');
+	if(id==''){
+		window.location.href = "../";
+	}
+	
+	$("#facebook-comments").attr("data-href", "http://storeelectronica.esy.es/producto.php?id="+id);
 
+	var parametros = {
+		'id':id
+	};
+	$.ajax
+	({
+		data:parametros,
+		url:"php/productos/getInfo.php",
+		type:"post",
+		dataType: 'json',
+		success:function(datos)
+		{
+			if(!datos.productos)
+			{
+				swal("Error", "No se encontro el producto.", "error");
+				setTimeout(function(){
+					location.href = "../";
+				}, 3000);
+			}
+			for(var i=0; i<datos.productos.length; i++)
+			{
+				var id = datos.productos[i].id;
+				var nombre = datos.productos[i].nombre;
+				var descripcion = datos.productos[i].descripcion;
+				var precio = datos.productos[i].precio;
+				var precioOLD = parseFloat(datos.productos[i].precio)+100;
+				var valoracion = datos.productos[i].estrellas;
+				var imagen = datos.productos[i].imagenes[0];
+				var estrellas = '';
+				for(var x=0; x<valoracion; x++){
+					estrellas += '<i class="fa fa-star"></i>';
+				}
+				
+				$("#img-main").attr("src", "./img/"+imagen);
+				//$("#img-thumb").attr("src", "./img/"+imagen);
+				
+				$("#product-name").html(nombre);
+				$("#product-rating").html(estrellas);
+				$("#product-price").html('$'+precio);
+				$("#product-old-price").html('$'+precioOLD);
+				$("#product-descripcion").html(descripcion);
+				
+				$("#add-to-cart-btn").attr("data-id", id);
+				$("#add-to-cart-btn").attr("data-imagen", imagen);
+				$("#add-to-cart-btn").attr("data-nombre", nombre);
+				$("#add-to-cart-btn").attr("data-precio", precio);
+				
+				$("#text-btn-loading").text("Añadir al carrito");
+				
+				loaded = true;
+			}
+			
+			// Product img zoom
+			var zoomMainProduct = document.getElementById('product-main-img');
+			if (zoomMainProduct) {
+				$('#product-main-img .product-preview').zoom();
+			}
+		}
+	});
+	
+	$("#add-to-cart-btn").on("click", function(e)
+	{
+		var id = $(this).data('id');
+		var imagen = $(this).data('imagen');
+		var nombre = $(this).data('nombre');
+		var precio = $(this).data('precio');
+		
+		if(loaded)
+		{
+			addCarrito(id, imagen, nombre, precio);
+		}
+		
+	});
+
+});
+</script>
 	</body>
 </html>
